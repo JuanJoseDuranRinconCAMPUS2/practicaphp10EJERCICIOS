802f3ce7e77a36adcaac4fd182fd95f9b2a586af
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-24 21:30:41 -0500

    feat: :sparkles: Ejercicio 7 (new Commits)
    realizacion del septimo punto del taller de javascript hecho con php

--- api.php
+++ api.php
@@ -1,46 +1,32 @@
 <?php
-    // Construir el algoritmo en Javascript para un programa
-    // para cualquier cantidad de estudiantes que lea el nombre,
-    // el sexo y la nota definitiva y halle al estudiante con la mayor
-    // nota y al estudiante con la menor nota y cuantos eran
-    // hombres y cuantos mujeres.
+//    Programa que pida el ingreso del nombre y precio de un artículo y la
+//    cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+//    en su factura.
 
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $_METHOD = $_SERVER["REQUEST_METHOD"];
 
-    function Forms($variable){
-        $html = ''; 
-
-        for ($i = 1; $i <= $variable; $i++) {
-            $estudianteHTML = '
-            <fieldset id="dataForm">
-                <legend>#Estudiante ' . $i . '</legend>
-                <label>Ingresa el nombre del estudiante </label><br>
-                <input type="text" name="Nombre_' . $i . '"   required><br><br>
-                <label>Ingresa el genero del estudiante </label><br>
-                <select name="Genero_' . $i . '" required>
-                    <option value="M">Hombre</option>
-                    <option value="F">Mujer</option>
-                </select><br><br>
-                <label>Ingresa la nota del estudiante </label><br>
-                <input type="text" name="Nota_' . $i . '"   required><br>
-            </fieldset><br><br>
-            ';
-    
-            $html .= $estudianteHTML;
-        }
-    
-        return $html;
-    }
-
+    function Total($_DATA){
+        $subtotal = $_DATA["Cproducto"] * $_DATA["Pproducto"];
+        $Iva = $subtotal * 0.19;
+        $total = $subtotal + $Iva;
+        return $total; 
+    };
     $validate = function($_DATA){
-        if (is_numeric($_DATA["estudiantes"])) {
-            session_start();
-            $variable = $_DATA["estudiantes"];
-            $_SESSION['#Estudiantes'] = $variable;
-            $mensaje = Forms($variable);
-            echo $mensaje;
+        if (is_numeric($_DATA["ID"]) && is_numeric($_DATA["Pproducto"]) && is_numeric($_DATA["Cproducto"])) {
+            $mensaje = '
+                <h3> Factura </h3>
+                <h4> Datos del comprador </h4>
+                <p>Comprador: ' . $_DATA["Cliente"] .'</p>
+                <p>TI: ' . $_DATA["ID"] .'</p>
+                <h4> Datos del producto </h4>
+                <p>Nombre_Del_Producto: ' . $_DATA["Nproducto"] .'</p>
+                <p>Cantidad_Del_Producto: ' . $_DATA["Cproducto"] .'</p>
+                <p>Precio_Del_Producto: ' . $_DATA["Pproducto"] .'</p>
+                <p>Total: ' . Total($_DATA);'</p>
+            ';
+        echo $mensaje;
         }else {
             $mensaje = "<h1>ERROR datos no compatibles, ingresa un numero</h1>";
             echo $mensaje;

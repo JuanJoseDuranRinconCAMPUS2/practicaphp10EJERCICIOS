6e84b265553dce80e10c6b6b25ee29c0bc71916e
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-25 04:59:16 -0500

    feat: :sparkles: Ejercicio 10 (new Commits)
    realizacion del decimo punto del taller de javascript hecho con php

--- api.php
+++ api.php
@@ -1,54 +1,59 @@
 <?php
-// N atletas han pasado a finales en salto triple en los juegos
-// olímpicos femenino de 2022. Diseñe un programa que pida por
-// teclado los nombres de cada atleta finalista y a su vez, sus
-// marcas del salto en metros. Informar el nombre de la atleta
-// campeona que se quede con la medalla de oro y si rompió
-// récord, reportar el pago que será de 500 millones. El récord
-// esta en 15,50 metros.
+// Desarrolle un programa cíclico que capture un dato
+// numérico cada vez, y los vaya acumulando. El programa se
+// detiene cuando el usuario digita un cero. El programa debe
+// mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+// PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+// VALOR Y MENOR VALOR.
 
-    header("Content-Type: application/json; charset:UTF-8");
-    $_DATA = json_decode(file_get_contents("php://input"), true);
-    $_METHOD = $_SERVER["REQUEST_METHOD"];
+header("Content-Type: application/json; charset:UTF-8");
+$_DATA = json_decode(file_get_contents("php://input"), true);
+$_METHOD = $_SERVER["REQUEST_METHOD"];
+    
 
-    function Forms($variable){
-        $html = ''; 
-
-        for ($i = 1; $i <= $variable; $i++) {
-            $estudianteHTML = '
-            <fieldset id="dataForm">
-                <legend>#Atleta ' . $i . '</legend>
-                <label>Ingresa el nombre de la atleta </label><br>
-                <input type="text" name="NombreA_' . $i . '" required><br><br>
-                <label>Ingresa la marca de la Atleta</label><br>
-                <input type="text" name="Marca_' . $i . '"  pattern="[0-9-.]+" required><br>
-            </fieldset><br><br>
-            ';
-
-            $html .= $estudianteHTML;
-        }
-
-        return $html;
+function operacionD($_DATA){
+    $datos = array();
+    foreach($_DATA as $i){
+        $dato = floatval($i['Numero']);
+        if (is_numeric($dato)) {
+            array_push($datos, $dato);
+        };
     }
+    $suma = array_sum($datos);
+    $numDatos = count($datos);
+    $lengthDatos = $suma / $numDatos;
+    $Min = min($datos);
+    $Max = max($datos);
+    if (in_array(0, $datos)){
+        return array(
+            "Suma" => $suma,
+            "Promedio" => $lengthDatos,
+            "lengthDatos" => $numDatos,
+            "Min" => $Min,
+            "Max" => $Max,
+        );
+    } 
+};
+function validate($_DATA){
+    if (is_array($_DATA)) {
+        $mensaje = (array) [
+            "Num" => $_DATA,
+            "operacionD" => operacionD($_DATA),
+        ];
+        echo json_encode($mensaje, JSON_PRETTY_PRINT);
+    }
+    
+    
+    
+};
 
-    $validate = function($_DATA){
-        if (is_numeric($_DATA["Atletas"])) {
-            session_start();
-            $Atletas = $_DATA["Atletas"];
-            $_SESSION['#Atletas'] = $Atletas;
-            $mensaje = Forms($Atletas);
-            echo $mensaje;
-        }else {
-            $mensaje = "<h1>ERROR datos no compatibles, ingresa un numero</h1>";
-            echo $mensaje;
-        };
-    };
+try {
+$res = match($_METHOD){
+    "POST" => validate($_DATA)
+};
+}catch (\Throwable $th) {
+    $res = "ERROR";
+};
 
-    try {
-        $res = match($_METHOD){
-            "POST" => $validate($_DATA)
-        };
-    }catch (\Throwable $th) {
-        $mensaje = "ERROR";
-    };
+   
 ?>
--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-//N atletas han pasado a finales en salto triple en los juegos
-// olímpicos femenino de 2022. Diseñe un programa que pida por
-// teclado los nombres de cada atleta finalista y a su vez, sus
-// marcas del salto en metros. Informar el nombre de la atleta
-// campeona que se quede con la medalla de oro y si rompió
-// récord, reportar el pago que será de 500 millones. El récord
-// esta en 15,50 metros.
+// Desarrolle un programa cíclico que capture un dato
+// numérico cada vez, y los vaya acumulando. El programa se
+// detiene cuando el usuario digita un cero. El programa debe
+// mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+// PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+// VALOR Y MENOR VALOR.
 
-let myFormUsuarios = document.querySelector("#myFormUsuarios");
+let myFormNumero = document.querySelector("#myFormNumero");
 let myHeaders = new Headers({"content-Type": "application/json"});
 let config = {
     headers : myHeaders,
 }
+let conjunto = [];
 
-myFormUsuarios.addEventListener("submit", async(e)=>{
+myFormNumero.addEventListener("submit", async(e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
     config.method = "POST";
-    config.body = JSON.stringify(data);
+    conjunto.push(data);
+    config.body = JSON.stringify(conjunto);
     let res = await (await fetch("api.php", config)).text();
-    document.querySelector("#content").innerHTML = res;
-    document.querySelector("#button").innerHTML = '<input type="submit" value="Hecho"></input>';
-});
-
-let myFormNombres = document.querySelector("#myFormNombres");
-
-myFormNombres.addEventListener("submit", async(e)=>{
-    e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target));
-    config.method = "POST";
-    config.body = JSON.stringify(data);
-    console.log(data);
-    let res = await (await fetch("atletasCalculo.php", config)).text();
     console.log(res);
     try {
         var resJS = JSON.parse(res);
         console.log(resJS);
+    
+        var numeros = resJS.Num.map(item => item.Numero);
+        var numerosString = numeros.join(", ");
+        console.log(numerosString);
         document.querySelector("#tableContent").innerHTML = `
-            <tr id="contenidoPersona">
-                <th>Medalla De Oro</th>
-                <td>${resJS.MedallaDeOro.Nombre}</td>
-                <td>${resJS.MedallaDeOro.Marca}</td>
-                <td>${resJS.Premiacion}</td>
+            <tr>
+                <th scope="col">Operaciones</th>
+                <td>${numerosString}</td>
+                <td>${resJS.operacionD.Suma}</td>
+                <td>${resJS.operacionD.Promedio}</td>
+                <td>${resJS.operacionD.lengthDatos}</td>
+                <td>${resJS.operacionD.Max}</td>
+                <td>${resJS.operacionD.Min}</td>
             </tr>
         `;
     } catch (error) {
         document.querySelector("#tableContent").innerHTML = `
             <tr id="contenidoPersona">
                 <th>Error</th>
-                <td>Revisa</td>
-                <td>la</td>
-                <td>data</td>
+                <td>para ver la data</td>
+                <td>Digita el</td>
+                <td>numero 0</td>
+                <td>ademas recuerda</td>
+                <td>Poner la data</td>
+                <td>requerida</td>
+
+
             </tr>
         `;
-    }
+    };
 });
